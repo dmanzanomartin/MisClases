91b3661c1c4c4ee49f9a3cbc65933161dfbd0dea
davidWindowsPere3 <davidmanzanomartin@gmail.com>
2021-03-02 22:07:19 +0100

    Arreglado CSVParser.js

--- JS/CSVParser/CSVParser.js
+++ JS/CSVParser/CSVParser.js
@@ -1,35 +1,49 @@
-class CSVParser(){
+class CSVParser{
 
-	data = [];
+    constructor (){}
     
-    get readFromFile(file, hasHeaders = true, separator = ";"){
-	    let reader = new FileReader();
-        let tagNames = [];
-        reader.onload = function(progress){
-        	var lines = this.result.split("\n");
-            for(let k=0; k<=lines.length-1;k++){
-            	if(!hasHeaders){
-                	data.push(lines[k].split(separator));
-                } else{
-                	if(k==0){
-                    	tagNames.push(lines[k].split(separator));
+    /**
+     * 
+     * @param {File} file - File to be readed
+     * @param {Boolean} hasHeaders - True or false depending if the file has headers
+     * @param {String} separator - String to define the type of separator
+     */
+    readFromFile(file, hasHeaders = false, separator = ";"){
+	    return new Promise((resolve, reject)=>{
+            let reader = new FileReader();
+            let headers = new Array();
+            let returnData = new Array();
+            returnData["data"] = new Array();
+            reader.onload = function(progress){
+            	var lines = this.result.split("\n");
+                for(let k=0; k<=lines.length-1;k++){
+                	if(!hasHeaders){
+                    	returnData["data"].push(lines[k].split(separator));
                     } else{
-                    	let els = lines[k].split(separator);
-                        let newObjt = [];
-                    	for(int i=0;i<=els.lenght-1;i++){
-                        	newObjt.push(tagNames[i]) = els[i];
+                    	if(k==0){
+                        	headers.push(lines[k].split(separator));
+                        } else{
+                        	let els = lines[k].split(separator);
+                            let newObjt = [];
+                        	for(let i=0;i<=els.length-1;i++){
+                            	newObjt[headers[0][i]] = els[i];
+                            }
+                            returnData["data"].push(newObjt);
                         }
-                        data.push(newObj);
                     }
                 }
+                if(hasHeaders){
+                    returnData["headers"] = headers[0];
+                }
+                resolve(returnData);
             }
-        }
-        try{
-        	reader.readAsText(file);
-        } catch e{
-        	return e;
-        }
-        return data;
+            try{
+            	reader.readAsText(file);
+                return returnData;
+            } catch (e){
+            	return new Array();
+            }
+        })
     }
 
 }
